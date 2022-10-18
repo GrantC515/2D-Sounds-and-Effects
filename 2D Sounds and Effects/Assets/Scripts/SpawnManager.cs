c68265d97c6b90d25c96b8a9aff34551a27c59e7
GrantC515 <111007253+GrantC515@users.noreply.github.com>
2022-10-18 11:05:25 -0700

    Caught up with anims and game over sequence

--- 2D Sounds and Effects/Assets/Scripts/SpawnManager.cs
+++ 2D Sounds and Effects/Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,23 @@ public class SpawnManager : MonoBehaviour
     private Vector3 _spawnPos = new Vector3(15, -3.5f, 0);
     private float _startDelay = 2;
     private float _repeatRate = 3;
+
+    private PlayerController _playerScript;
+
     // Start is called before the first frame update
     void Start()
     {
+        _playerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", _startDelay, _repeatRate);
     }
 
     // Update is called once per frame
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, _spawnPos, obstaclePrefab.transform.rotation);
+        if(_playerScript.isGameOver == false)
+        {
+            Instantiate(obstaclePrefab, _spawnPos, obstaclePrefab.transform.rotation);
+        }
+       
     }
 }
